6009053ae7651a07a2ee54386033dabb3b6de853
chris_laptop <basketcristian@gmail.com>
2023-10-02 14:55:20 +0200

    [ADD] Ep2 : - delta is used when the game is lagging , and u loose the frame and he wond move , we multiply the velocity for the delta (Delta is way to small so we need to also add the VELOCITY value we want so we add MAX_SPEED) - normalize the diagonal velocity movement with .normalized() - adding accelleration - adding friction -> we want to stop slowly and not instantly , so  we want the vector velocity to slowly reaching Zero  so we use method .move_toward(<where>,how slowly) - adding CAP to the velocity with clamp (-MAX,MAX) ->we can set a min velocity and Max Velocity ( MAX must be a vector2 )

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,5 @@
 extends CharacterBody2D
 
-
-
 #func _physics_process(delta):
 #	print("Start")
 #	if Input.is_action_pressed("ui_left"):
@@ -12,16 +10,22 @@ extends CharacterBody2D
 #		velocity.x = 0
 #		velocity.y = 0
 #	move_and_collide(velocity)
-#
+
+const MAX_SPEED = 300
+const ACCELERATION = 30
+const FRICTION = 30
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
-	else:
-		velocity = Vector2.ZERO
+		velocity += input_vector * ACCELERATION * delta
 		
-	move_and_collide(velocity)
+		velocity = velocity.clamp(Vector2(-MAX_SPEED*delta,-MAX_SPEED*delta), Vector2(MAX_SPEED*delta,MAX_SPEED*delta))
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta ) 
+		
+	move_and_collide(velocity )
