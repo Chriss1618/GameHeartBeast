28240718bd6fb2be1a7ea581f530237e20b7e56d
chris_laptop <basketcristian@gmail.com>
2023-10-02 17:48:46 +0200

    [ADD] Ep 4 : - make a node as a Scene so it became modular, so that we can reuse it in other scenes -> RClick on the node and select Save Branch as Scene -> in this way we can drag from where we saved to the new scene and reuse it. - difference between creating a new scene liek 2D Node or Base Node -> when dragging Scene Objects with 2DNoce they dont jump to the position 0,0 - change Scene Node -> enable Ysort in his Inspectr so we can view items above others if they are positioned in front of it - remember to move properly the collision and the spirte to the center of his father Node -> in our example our player Scene has his sprite and collider positionade not at the center of the father node Player. this is coz our Ysort is managed on this positions of objects , the Y sorting is on the CENTER of the  NODE so if we position the spirte not in a correct position of his node we can see weird Y sort Layers

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,16 +1,5 @@
 extends CharacterBody2D
 
-#func _physics_process(delta):
-#	print("Start")
-#	if Input.is_action_pressed("ui_left"):
-#		velocity.x = -4
-#	elif Input.is_action_pressed("ui_right"):
-#		velocity.x = 4
-#	else :
-#		velocity.x = 0
-#		velocity.y = 0
-#	move_and_collide(velocity)
-
 const MAX_SPEED = 100
 const ACCELERATION = 500
 const FRICTION = 500
@@ -22,12 +11,8 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		#velocity += input_vector * ACCELERATION * delta
-		#velocity = velocity.clamp(Vector2(-MAX_SPEED,-MAX_SPEED), Vector2(MAX_SPEED,MAX_SPEED)) 
 		velocity = velocity.move_toward( input_vector * MAX_SPEED, ACCELERATION * delta ) 
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta ) 
-		
-	#move_and_collide( velocity * delta )
 	
 	move_and_slide()
