13f1aad77961ff1158498d86733e83c78e6bdc83
chris_laptop <basketcristian@gmail.com>
2023-10-05 11:25:09 +0200

    [PROJECT CLOSED]

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,17 +3,28 @@ extends CharacterBody2D
 const MAX_SPEED = 100
 const ACCELERATION = 500
 const FRICTION = 600
+enum{
+	MOVE, 	#0
+	ROLL, 	#1
+	ATTACK 	#2
+}
+
+var state 
 
 @onready var animationPlayer = $AnimationPlayer 
 @onready var animationTree = $AnimationTree
 @onready var animationState = animationTree.get("parameters/playback")
+
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") 
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	
+
 	input_vector = input_vector.normalized()
-	
+
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position",input_vector)
 		animationTree.set("parameters/Run/blend_position",input_vector)
@@ -22,5 +33,5 @@ func _physics_process(delta):
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta ) 
-	
+
 	move_and_slide()
